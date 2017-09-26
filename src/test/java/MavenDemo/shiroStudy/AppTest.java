86dd1d65ba2413411a249a8eedb1542083f6a06e
meifuqiao <1240457676@qq.com>
2017-09-26 15:06:14 +0800

    sf

--- src/test/java/MavenDemo/shiroStudy/AppTest.java
+++ src/test/java/MavenDemo/shiroStudy/AppTest.java
@@ -1,38 +1,40 @@
 package MavenDemo.shiroStudy;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import javax.security.auth.Subject;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest{
+ 
+  
+    
+    @Test
+    public void testHelloWord(){
+    	
+    	Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini"); 
+    	org.apache.shiro.mgt.SecurityManager  securityManager= factory.getInstance();
+    	SecurityUtils.setSecurityManager(securityManager);
+    	org.apache.shiro.subject.Subject subject = SecurityUtils.getSubject();
+    	UsernamePasswordToken token = new UsernamePasswordToken("zhang","123");
+    	try{
+    		subject.login(token);
+    	}catch (AuthenticationException e) {
+    		System.out.println("Login Failed");
+    	}
+    	Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录  
+    	  
+        //6、退出  
+        subject.logout();  
+    
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
